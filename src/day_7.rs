d72915a51953e50613c4f144f253300d597d3c42
Thomas Wiringa <thomas@tjwiringa.nl>
2024-12-07 13:34:37 +0100

    Finished day 7

--- src/day_7.rs
+++ src/day_7.rs
@@ -8,7 +8,7 @@ pub fn execute() {
     utils::print_day_banner(7);
 
     println!("Part 1: {}", part1(&input));
-    // println!("Part 2: {}", part2(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 fn part1(input: &str) -> i64 {
@@ -16,14 +16,20 @@ fn part1(input: &str) -> i64 {
 
     items
         .iter()
-        .filter(|(k, v)| check_combination(*k, 0, v))
+        .filter(|(k, v)| check_combination(*k, v[0], &v[1..], false))
         .map(|item| item.0)
         .sum()
 }
 
-// fn part2(input: &str) -> i32 {
-//     //
-// }
+fn part2(input: &str) -> i64 {
+    let items = parse_input(input);
+
+    items
+        .iter()
+        .filter(|(k, v)| check_combination(*k, v[0], &v[1..], true))
+        .map(|item| item.0)
+        .sum()
+}
 
 // Build a list of numbers like (190, [10, 19])
 fn parse_input(input: &str) -> Vec<(i64, Vec<i64>)> {
@@ -33,7 +39,7 @@ fn parse_input(input: &str) -> Vec<(i64, Vec<i64>)> {
         .map(|line| {
             let (left, right) = line.split_once(": ").unwrap();
             let parts = right
-                .split(" ")
+                .split_whitespace()
                 .map(|i| i64::from_str(i).unwrap())
                 .collect();
 
@@ -42,19 +48,26 @@ fn parse_input(input: &str) -> Vec<(i64, Vec<i64>)> {
         .collect()
 }
 
-fn check_combination(target: i64, current: i64, numbers: &[i64]) -> bool {
+fn check_combination(target: i64, current: i64, numbers: &[i64], with_concat: bool) -> bool {
     if numbers.is_empty() {
         return current == target;
     }
 
-    if current >= target {
+    if current > target {
         return false;
     }
 
     let first_value = *numbers.first().unwrap();
 
-    check_combination(target, current + first_value, &numbers[1..])
-        || check_combination(target, current * first_value, &numbers[1..])
+    check_combination(target, current + first_value, &numbers[1..], with_concat)
+        || check_combination(target, current * first_value, &numbers[1..], with_concat)
+        || (with_concat
+            && check_combination(
+                target,
+                format!("{}{}", current, first_value).parse().unwrap(),
+                &numbers[1..],
+                with_concat,
+            ))
 }
 
 #[cfg(test)]
@@ -68,8 +81,8 @@ mod tests {
         assert_eq!(part1(TEST_INPUT), 3749);
     }
 
-    // #[test]
-    // pub fn test_part2() {
-    //     assert_eq!(part2(TEST_INPUT), 6);
-    // }
+    #[test]
+    pub fn test_part2() {
+        assert_eq!(part2(TEST_INPUT), 11387);
+    }
 }
