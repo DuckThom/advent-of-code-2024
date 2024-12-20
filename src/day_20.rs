52920350c347103df512bd19abd991a06ba6c19a
Thomas Wiringa <t.wiringa@youweagency.com>
2024-12-20 14:49:33 +0100

    Refactoring but D20p2 not working yet

--- src/day_20.rs
+++ src/day_20.rs
@@ -1,24 +1,22 @@
 use pathfinding::prelude::dijkstra;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub fn execute(input: &str) {
-    println!("Part 1: {}", part1(&input, 100));
-    // println!("Part 2: {}", part2(&input));
+    println!("Part 1: {}", run(&input, 100, 2));
+    println!("Part 2: {}", run(&input, 100, 20));
 }
 
-fn part1(input: &str, required_time_save: usize) -> usize {
+type Maze = Vec<Vec<bool>>;
+
+fn run(input: &str, required_time_save: usize, skippable: usize) -> usize {
     let (start, end, mut maze) = parse_input(input);
 
     block_dead_ends(&mut maze, &start, &end);
 
-    let cheats: HashMap<usize, usize> = cheat_the_maze(&mut maze, &start, &end);
+    let cheats: HashMap<usize, usize> = cheat_the_maze(&mut maze, &start, &end, skippable);
 
     count_cheats(&cheats, &required_time_save)
 }
-
-// fn part2(input: &str) -> usize {
-//
-// }
 
 fn count_cheats(cheats: &HashMap<usize, usize>, required_time_save: &usize) -> usize {
     cheats.into_iter().fold(0, |acc, (score, count)| {
@@ -30,63 +28,53 @@ fn count_cheats(cheats: &HashMap<usize, usize>, required_time_save: &usize) -> u
     })
 }
 
-fn cheat_the_maze(maze: &Vec<Vec<bool>>, start: &Pos, end: &Pos) -> HashMap<usize, usize> {
-    let (_, score) = do_the_dijkstra_thing(&maze, &start, &end).unwrap();
+fn cheat_the_maze(maze: &Maze, start: &Pos, end: &Pos, skippable: usize) -> HashMap<usize, usize> {
+    let (true_path, score) = do_the_dijkstra_thing(&maze, &start, &end).unwrap();
+
     let mut cheats: HashMap<usize, usize> = HashMap::new();
+    let mut visited: HashSet<Pos> = HashSet::new();
 
-    maze.iter().enumerate().for_each(|(y, row)| {
-        if y == 0 || y == maze.len() - 1 {
-            return;
-        }
+    true_path.iter().for_each(|p| {
+        visited.insert(*p);
 
-        row.iter().enumerate().for_each(|(x, cell)| {
-            if x == 0 || x == row.len() - 1 {
-                return;
-            }
+        let jumpable_targets = p.can_jump_to(&true_path, &skippable);
 
-            if !cell {
-                let mut cheated_maze = maze.clone();
-                cheated_maze[y][x] = true;
+        for target in jumpable_targets {
+            if !visited.contains(&target) {
+                let start_index = true_path.iter().position(|p| p == &target).unwrap();
+                let cheated_path = true_path[start_index..].to_vec();
 
-                let (_, cheated_score) =
-                    do_the_dijkstra_thing(&cheated_maze, &start, &end)
-                        .unwrap();
-
-                if cheated_score < score {
-                    cheats.entry(score - cheated_score).or_insert(0);
-                    cheats.entry(score - cheated_score).and_modify(|count| {
+                let cs = visited.len() + cheated_path.len();
+                if cs < score {
+                    cheats.entry(score - cs).or_insert(0);
+                    cheats.entry(score - cs).and_modify(|count| {
                         *count += 1;
                     });
                 }
             }
-        })
+        }
     });
 
     cheats
 }
 
 fn do_the_dijkstra_thing(
-    maze: &Vec<Vec<bool>>,
+    maze: &Maze,
     start: &Pos,
     end: &Pos,
 ) -> Option<(Vec<Pos>, usize)> {
-    dijkstra(start, |p| p.successors(&maze), |p| p == end)
-}
-
-
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-enum Direction {
-    Up,
-    Down,
-    Right,
-    Left,
+    dijkstra(
+        start,
+        |p| p.successors(&maze),
+        |p| p == end,
+    )
 }
 
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Pos(usize, usize);
 
 impl Pos {
-    fn successors(&self, maze: &Vec<Vec<bool>>) -> Vec<(Pos, usize)> {
+    fn successors(&self, maze: &Maze) -> Vec<(Pos, usize)> {
         let &Pos(x, y) = self;
         let mut next: Vec<Pos> = vec![];
 
@@ -94,7 +82,7 @@ impl Pos {
             next.push(Pos(x, y - 1));
         }
 
-        if maze[y + 1][x] {
+        if y < maze.len() && maze[y + 1][x] {
             next.push(Pos(x, y + 1));
         }
 
@@ -102,34 +90,72 @@ impl Pos {
             next.push(Pos(x - 1, y));
         }
 
-        if x < maze[y].len() - 1 && maze[y][x + 1]{
+        if x < maze[y].len() - 1 && maze[y][x + 1] {
             next.push(Pos(x + 1, y));
         }
 
         next.into_iter().map(|p| (p, 1)).collect()
     }
-}
 
-fn print_maze(maze: &Vec<Vec<bool>>, start: &Pos, end: &Pos, path: &Vec<Pos>) {
-    for (y, row) in maze.iter().enumerate() {
-        for (x, cell) in row.iter().enumerate() {
-            if *start == Pos(x, y) {
-                print!("S");
-            } else if *end == Pos(x, y) {
-                print!("E");
-            } else {
-                if path.iter().find(|p| **p == Pos(x, y)).is_some() {
-                    print!("*");
-                } else {
-                    print!("{}", if *cell { '.' } else { '█' });
+    fn can_jump_to(&self, path: &Vec<Pos>, skippable: &usize) -> HashSet<Pos> {
+        let &Pos(x, y) = self;
+
+        let mut jumpable_positions: HashSet<Pos> = HashSet::new();
+
+        for dy in 0..=*skippable {
+            let dx = *skippable - dy;
+
+            let candidates = vec![
+                (x.wrapping_sub(dx), y.wrapping_sub(dy)), // Top-left
+                (x + dx, y.wrapping_sub(dy)),             // Top-right
+                (x.wrapping_sub(dx), y + dy),             // Bottom-left
+                (x + dx, y + dy),                         // Bottom-right
+            ];
+
+            for (nx, ny) in candidates {
+                let new_pos = Pos(nx, ny);
+
+                if path.contains(&new_pos) {
+                    jumpable_positions.insert(new_pos);
                 }
             }
+        }
+
+        jumpable_positions
+    }
+}
+
+use colored::Colorize;
+
+#[allow(dead_code)]
+fn print_maze(maze: &Maze, start: &Pos, end: &Pos, path: &Vec<Pos>, jumpable_positions: &Vec<Pos>) {
+    for (y, row) in maze.iter().enumerate() {
+        for (x, cell) in row.iter().enumerate() {
+            let char_to_print: &str;
+
+            if *start == Pos(x, y) {
+                char_to_print = "S";
+            } else if *end == Pos(x, y) {
+                char_to_print = "E";
+            } else {
+                if path.iter().find(|p| **p == Pos(x, y)).is_some() {
+                    char_to_print = "*";
+                } else {
+                    char_to_print = if *cell { "." } else { "█" };
+                }
+            }
+
+            print!("{}", if jumpable_positions.contains(&Pos(x, y)) {
+                Colorize::red("J")
+            } else {
+                Colorize::white(char_to_print)
+            });
         }
         print!("\n");
     }
 }
 
-fn block_dead_ends(maze: &mut Vec<Vec<bool>>, start: &Pos, end: &Pos) {
+fn block_dead_ends(maze: &mut Maze, start: &Pos, end: &Pos) {
     let mut has_changes = true;
     let rows = maze.len();
 
@@ -160,7 +186,7 @@ fn block_dead_ends(maze: &mut Vec<Vec<bool>>, start: &Pos, end: &Pos) {
     }
 }
 
-fn count_paths(maze: &Vec<Vec<bool>>, (x, y): (usize, usize)) -> usize {
+fn count_paths(maze: &Maze, (x, y): (usize, usize)) -> usize {
     let mut paths: usize = 0;
 
     if maze[y - 1][x] {
@@ -182,7 +208,7 @@ fn count_paths(maze: &Vec<Vec<bool>>, (x, y): (usize, usize)) -> usize {
     paths
 }
 
-fn parse_input(input: &str) -> (Pos, Pos, Vec<Vec<bool>>) {
+fn parse_input(input: &str) -> (Pos, Pos, Maze) {
     let mut start: Pos = Pos(0, 0);
     let mut end: Pos = Pos(0, 0);
 
@@ -218,12 +244,11 @@ mod tests {
 
     #[test]
     pub fn test_part1() {
-        assert_eq!(part1(TEST_INPUT, 10), 10);
+        assert_eq!(run(TEST_INPUT, 10, 2), 10);
     }
 
-    // #[test]
-    // pub fn test_part2() {
-    //     assert_eq!(part2(SMALL_TEST_INPUT), 45);
-    //     assert_eq!(part2(LARGE_TEST_INPUT), 64);
-    // }
+    #[test]
+    pub fn test_part2() {
+        assert_eq!(run(TEST_INPUT, 50, 20), 45);
+    }
 }
